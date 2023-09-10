fd302f780c339fee54161ad8395d4ae4e8f45eb1
Dhivya <144351142+D07102004@users.noreply.github.com>
2023-09-10 19:45:15 +0530

    Create channege1.2 main.py

---
+++ channege1.2 main.py
@@ -0,0 +1,6 @@
+# 1. Python program to check the leap year 
+year = int (input("Enter year to be checked :"))
+if(year%4==0):
+  print("The given year is leap year")
+else:
+  print("The given year is not a leap year ")
